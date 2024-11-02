e868ebe2428a34099945ffff96bda036046999f5
guirs51 <143645273+guirs51@users.noreply.github.com>
2024-11-01 21:23:43 -0300

    Add files via upload

---
+++ trabalho1/index.js
@@ -0,0 +1,11 @@
+// perguntas 
+// 1: Back-end é oque faz funcionar tudo, é a parte de trás, o não visual que é extremamente esseencial.
+// 2: 
+// 3: São bibliotecas que tem varias ferramentas para o desenvolvimento de aplicações
+// 4: A API liga os dois 
+// 5:
+// 6;
+// 7:
+// 8: Porque é a parte, na minha opinião, que é mais essencial. E que eu sinto que estou programando de verdade. 
+
+// codigos
